3259b46eb4bf03799b8d0f97409112bd3266de69
HuanqingDeng <1145349052@qq.com>
2020-03-16 10:45:25 +0800

    添加了数据监控地图

--- src/pages/DateAnly/ChargeMonitor/index.jsx
+++ src/pages/DateAnly/ChargeMonitor/index.jsx
@@ -1,11 +1,23 @@
-import { Map } from 'react-amap'
+import { Map, Markers } from 'react-amap'
 import React, {useEffect, useState} from 'react'
 import { Card, DatePicker, LocaleProvider } from "antd";
 function ChargeMonitor() {
     const [center, setCenter] = useState({longitude:106.206478608,latitude:38.5026210119})
+    const [markers, setMarkers] = useState([
+        {position:[106.206478608,38.5026210119]},
+        {position:[104.776071339,29.3591568895]},
+        {position:[126.435797675,41.945859397]},
+        {position:[102.545067892,24.3704471344]},
+        {position:[124.391382074,43.1755247011]},
+        {position:[113.307649675,23.1200491021]},
+    ])
     return(
         <div style={{width: '100%', height: 550}}>
-            <Map amapkey={'171ce56dfa484dd533e1ac9be13faee5'} center={center} zoom={4}/>
+            <Map plugins={['ToolBar']} amapkey={'171ce56dfa484dd533e1ac9be13faee5'} center={center} zoom={4} mapStyle='fresh'>
+                <Markers 
+                    markers={markers}
+                />
+            </Map>
         </div>
     )
 }
